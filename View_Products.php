a564673972f53133c76737ad568af88ee6182ab4
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 18:01:55 -0400

    Update View_Products.php

--- View_Products.php
+++ View_Products.php
@@ -1,35 +1,11 @@
 <?php
-//Execute the code found in (database_mysqli.php to connect to the database and makes sure the script runs once).
-require_once('database_mysqli.php');
-
-    /*
-        Code is referenced from Murachs PHP and MySQL.    
-    */
-    //[WHERE database_column_data = :user_entered_data]
     $query = 'SELECT id, name, holiday, type, description, stock, price, sku FROM products ORDER BY sku';
     
-    //Prepare the statement
     $statement = $db->prepare($query);
     
-    //Bind the columns in the result set to variables that will access there values.(Used when Displaying Information of each user)
     $statement ->bind_result($id,$name,$holiday,$type, $description, $stock, $price, $sku);
     
     $statement ->execute();
-    
-    /*[For the forms input]
-        The name attribute for the forms will all be the same.
-        PHP allows to combine the values of multiple checkboxes into a single array.(The same name is used for all related checkboxes)
-        Indicated with a bracket[] to show that check boxes  will be stored in an array.(Ex: name = 'user_role[]')
-        
-        When the form data is submitted, PHP creates an array when it reads the first check box name that ends with a bracket.Then it adds the value stored in the inputs value attribute(check box) to the array.
-        
-        If there is another checkbox, it will append that value to the array.(Once done PHP will nest the array within the $_GET or $_POST array)
-        
-        The values in the inputs for the checkboxes is used to identify the checkbox and indicate that it is checked.(This value corresponds to the primary key of the data in the database. For example: user_id)
-        
-        Each user has a unique user_id, so this is good for the value attribute.(The value can be used to obtain more information about the selected textbox)
-        
-    */
     
 echo 
 "<!DOCTYPE html>
