f0424dd7eabc3fb15bbc00b2e9c910063fa280fa
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 18:07:54 -0400

    Update process_product_edit.php

--- process_product_edit.php
+++ process_product_edit.php
@@ -16,17 +16,13 @@
     //If zero then the SQL statement was a success.(Executes properly)
     $is_execution_successful = 0;
     
-    //Binds values to the parameters.
-    //Binds the value to the parameter.(string format: s since there is one string argument)
+    
     $statement->bind_param("idii",$stock, $price, $sku, $product_id);
         
-    //Executes the SQL statement.(Updates the data, but does not return a result set)
     $is_execution_successful  = $statement->execute();
     
-    //Check to make sure that the SQL operation was successful.
     if($is_execution_successful)
     {
-        //Get number of affected rows.
         $number_of_rows_affected = $db->affected_rows;
         echo "<p>The number of rows in the table removed: $number_of_rows_affected</p>";
         
@@ -37,10 +33,4 @@
         $error_message = $db->$error;
         echo "<p>An error occurred: $error_message</p>";
     }
-    
-    //Free the resources.
-    $statement->close();
-    
-    //Disconnect from server.
-    $db->close();
 ?>
