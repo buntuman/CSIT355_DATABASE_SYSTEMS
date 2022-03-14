fd94af07c03eba27db9492bc65bf2168fbc6da70
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 18:22:31 -0400

    Update Delete_Product.php

--- Delete_Product.php
+++ Delete_Product.php
@@ -3,21 +3,6 @@
 require_once('database_mysqli.php');
 
     
-    //If zero then the SQL statement was a success.(Executes properly)
-    $is_execution_successful = 0;
-    
-    //Key starts at the value 0..to the last index of the array.(value is the value stored at that specific index.Association)
-    //(EX: [$key:0, $value: role_id])
-    foreach($product_names as $key => $value)
-    {
-        //Binds values to the parameters.
-        //Binds the value to the parameter.(string format: s since there is one string argument)
-        $statement->bind_param("s",$value);
-        
-        //Executes the SQL statement.(Updates the data, but does not return a result set)
-        $is_execution_successful  = $statement->execute();
-    }
-    
     //Check to make sure that the SQL operation was successful.
     if($is_execution_successful)
     {
