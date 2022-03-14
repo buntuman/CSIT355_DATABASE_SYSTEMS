dd7a5ed7e74e8cf07a7c5018329ecc55be295ec0
Joseph-Pepe <32807576+Joseph-Pepe@users.noreply.github.com>
2022-03-14 17:44:00 -0400

    Update process_login_mysqli.php

--- process_login_mysqli.php
+++ process_login_mysqli.php
@@ -9,17 +9,12 @@ require_once('database_mysqli.php');
     */
     //With SQL Join
     $query = "SELECT username, passwor_d, role_id, role_description FROM USERS,ROLE WHERE username = ? AND passwor_d = ? AND role_description = ? AND role_id = user_role_id"; 
-         
-    //Returns an object.       
-    //Prepare() statement improves both database security and performance.                       
-    $statement = $db->prepare($query);
     
-    //Binds $user_name and $user_password.
     //Binds the value to the parameter.(string format: s, and ss since there are two string arguments)
 	/*[Without SQL Join]
 	    $statement->bind_param("ss",$user_name, $user_password);
 	*/
-	//With SQL JOIN
+	// With SQL JOIN
 	$statement->bind_param("sss",$user_name, $user_password, $user_role);
 	
 	//Bind the columns in the result set to the variables that provide access to their values.(Binds username and passwor_d to the parameters $username, $password, $role_id)
